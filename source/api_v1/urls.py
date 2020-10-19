1ffb6927274c89c12f06b45378ec682175b59483
Aitkeeva Aizhan <aitkeeva.aizhan@gmail.com>
2020-10-19 13:58:41 +0600

    Добавление View Update

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from api_v1.views import get_token_view, ArticleListView, ArticleCreateView
+from api_v1.views import get_token_view, ArticleListView, ArticleCreateView, ArticleDetailView, ArticleUpdateView
 
 app_name = 'api_v1'
 
 urlpatterns = [
     path('get-token/', get_token_view, name='get_token'),
     path('articles/', ArticleListView.as_view(), name='article_list'),
-    path('article/create/', ArticleCreateView.as_view(), name='article_create')
+    path('article/create/', ArticleCreateView.as_view(), name='article_create'),
+    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article_view'),
+    path('article/<int:pk>/', ArticleUpdateView.as_view(), name='article_update_view'),
 ]
--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -2,9 +2,11 @@ import json
 from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
 from django.views.generic import View
 from django.views.decorators.csrf import ensure_csrf_cookie
+from rest_framework.views import APIView
 
 from api_v1.serializers import ArticleSerializer
 from webapp.models import Article
+from rest_framework.response import Response
 
 
 @ensure_csrf_cookie
@@ -32,3 +34,42 @@ class ArticleCreateView(View):
             response = JsonResponse(slr.errors, safe=False)
             response.status_code = 400
             return response
+
+# class ArticleCreateView(APIView):
+#     def post(self, request, *args, **kwargs):
+#         serializer = ArticleSerializer(data=request.data)
+#         if serializer.is_valid():
+#             article = serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors, status=400)
+#
+#
+# class ArticleListView(APIView):
+#     def get(self, request, *args, **kwargs):
+#         objects = Article.objects.all()
+#         serializer = ArticleSerializer(objects, many=True)
+#         return Response(serializer.data)
+
+
+class ArticleDetailView(View):
+    def get(self, request, *args, **kwargs):
+        article = Article.objects.get(pk=kwargs['pk'])
+        slr = ArticleSerializer(article, many=False)
+        return JsonResponse(slr.data, safe=False)
+
+
+class ArticleUpdateView(View):
+    def put(self, request, *args, **kwargs):
+        article = Article.objects.get(pk=kwargs['pk'])
+        data = json.loads(request.body)
+        slr = ArticleSerializer(data=data)
+        if slr.is_valid():
+            article = slr.save()
+            return JsonResponse(slr.data, safe=False)
+        else:
+            response = JsonResponse(slr.errors, safe=False)
+            response.status_code = 400
+            return response
+
+
