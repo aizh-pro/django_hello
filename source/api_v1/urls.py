c437d0d89f40bc8ef65288d97a16d50f72d90902
Aitkeeva Aizhan <aitkeeva.aizhan@gmail.com>
2020-10-19 16:09:15 +0600

    Добавление всех view

--- source/api_v1/urls.py
+++ source/api_v1/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from api_v1.views import get_token_view, ArticleListView, ArticleCreateView, ArticleDetailView, ArticleUpdateView
+from api_v1.views import get_token_view, ArticleListView, ArticleCreateView, ArticleDetailView, ArticleUpdateView, \
+    ArticleFullView, ArticleDeleteView
 
 app_name = 'api_v1'
 
@@ -8,6 +9,8 @@ urlpatterns = [
     path('get-token/', get_token_view, name='get_token'),
     path('articles/', ArticleListView.as_view(), name='article_list'),
     path('article/create/', ArticleCreateView.as_view(), name='article_create'),
-    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article_view'),
-    path('article/<int:pk>/', ArticleUpdateView.as_view(), name='article_update_view'),
+    path('article/detail/<int:pk>/', ArticleDetailView.as_view(), name='article_view'),
+    path('article/update/<int:pk>/', ArticleUpdateView.as_view(), name='article_update_view'),
+    path('article/detele/<int:pk>/', ArticleDeleteView.as_view(), name='article_delete_view'),
+    path('article/<int:pk>/', ArticleFullView.as_view(), name='article_full_view'), # здесь в одном view можно удалить, просмотреть, изменить статью
 ]
--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,7 +1,8 @@
 import json
-from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse, Http404
 from django.views.generic import View
 from django.views.decorators.csrf import ensure_csrf_cookie
+from rest_framework import status
 from rest_framework.views import APIView
 
 from api_v1.serializers import ArticleSerializer
@@ -63,7 +64,7 @@ class ArticleUpdateView(View):
     def put(self, request, *args, **kwargs):
         article = Article.objects.get(pk=kwargs['pk'])
         data = json.loads(request.body)
-        slr = ArticleSerializer(data=data)
+        slr = ArticleSerializer(data=data, instance=article)
         if slr.is_valid():
             article = slr.save()
             return JsonResponse(slr.data, safe=False)
@@ -71,5 +72,40 @@ class ArticleUpdateView(View):
             response = JsonResponse(slr.errors, safe=False)
             response.status_code = 400
             return response
+
+
+class ArticleDeleteView(View):
+    def delete(self,request, *args, **kwargs):
+        print('Номер статьи')
+        print(kwargs['pk'])
+        article = Article.objects.get(pk=kwargs['pk'])
+        article.delete()
+        return JsonResponse(article.pk)
+
+
+class ArticleFullView(APIView):
+    def get_object(self, pk):
+        try:
+            return Article.objects.get(pk=pk)
+        except Article.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        article = self.get_object(pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
